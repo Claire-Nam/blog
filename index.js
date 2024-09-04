862af13bf23ad755fa18a4dae03f5c169de703ff
Claire <nbr1023@naver.com>
2024-09-04 17:06:44 +0900

    내브바 미디어 쿼리 조정 필요 & 햄버거 삽입 필요 & 로그인 아이콘 위치 조정 필요

---
+++ index.js
@@ -0,0 +1,6 @@
+document
+  .getElementById("hamburger-menu")
+  .addEventListener("click", function () {
+    this.classList.toggle("active");
+    document.querySelector(".menu").classList.toggle("show");
+  });
