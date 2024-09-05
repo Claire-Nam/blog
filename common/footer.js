81bbba51e2369adacbcc690e6c9006adfce19f37
Claire <nbr1023@naver.com>
2024-09-05 17:14:43 +0900

    공통 요소 컴포넌트화 및 페이지 추가

---
+++ common/footer.js
@@ -0,0 +1,9 @@
+window.__globalFooter = `
+         <footer id="footer">
+      <img src="image/blog.jpg" />
+      <div class="footer-body">
+        <h4>최신 소식을 계속 듣고 싶다면?</h4>
+        <p>최신 업데이트 뉴스를 보내드립니다.</p>
+        <button type="submit">Subscribe</button>
+      </div>
+    </footer>`;
